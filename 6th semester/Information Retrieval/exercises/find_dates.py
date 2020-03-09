dcf3d7f8ea9f6348e6ab437eedef110be9e4ac58
xrisaD <xrdikonimaki@hotmail.com>
2020-03-09 19:37:18 +0200

    Update find_dates.py

--- 6th semester/Information Retrieval/exercises/find_dates.py
+++ 6th semester/Information Retrieval/exercises/find_dates.py
@@ -11,14 +11,13 @@ months = ""
 for i in range(1,13):
     month = calendar.month_name[i]
     month_short_form = month[:3]
-    
     if(i==12):
         months += month + "|" + month_short_form
         break
     months += month + "|" + month_short_form + "|"
 
 #find dates 
-dates_type_1 = re.findall("(([0-9]|[0-2][0-9]|3[0-1]) ("+months+") \d+)", doc)
+dates_type_1 = re.findall("(([0-9]|[0-2][0-9]|3[0-1]) (" + months + ") \d+)", doc)
 dates_type_2 = re.findall("(([0-9]|[0-2][0-9]|3[0-1])(-|/)([0-9]|0[0-9]|1[0-2])(-|/)\d+)"
                , doc)
 dates_1 = [tuple(i)[0] for i in dates_type_1]
