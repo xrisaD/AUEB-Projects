d60d386fd0fe3cfd981678d8b38bb022ebf73b35
xrisaD <xrdikonimaki@hotmail.com>
2020-03-11 19:20:54 +0200

    Add files via upload

--- 6th semester/Information Retrieval/exercises/find_dates.py
+++ 6th semester/Information Retrieval/exercises/find_dates.py
@@ -1,29 +1,39 @@
-#exercise 4.6
 import re
 import calendar
+from urllib.request import urlopen
+
+doc = urlopen("https://www.ted.com/tedx/events").read()
+doc = str(doc)
 
 #read file and create a string
+"""
 with open('document.txt', 'r') as file:
     doc = file.read().replace('\n', ' ')
+"""
+
 
 #create regex with months
 months = ""
 for i in range(1,13):
     month = calendar.month_name[i]
     month_short_form = month[:3]
+    
     if(i==12):
         months += month + "|" + month_short_form
         break
     months += month + "|" + month_short_form + "|"
 
 #find dates 
-dates_type_1 = re.findall("(([0-9]|[0-2][0-9]|3[0-1]) (" + months + ") \d+)", doc)
+dates_type_1 = re.findall("(([0-9]|[0-2][0-9]|3[0-1]) ("+months+") \d+)", doc)
 dates_type_2 = re.findall("(([0-9]|[0-2][0-9]|3[0-1])(-|/)([0-9]|0[0-9]|1[0-2])(-|/)\d+)"
-               , doc)
+                          , doc)
+dates_type_3 = re.findall("(("+months+") ([0-9]|[0-2][0-9]|3[0-1]), \d+)",doc)
+                          
 dates_1 = [tuple(i)[0] for i in dates_type_1]
 dates_2 = [tuple(i)[0] for i in dates_type_2]
-
+dates_3 = [tuple(i)[0] for i in dates_type_3]
+                          
 #print dates
 print(dates_1)
 print(dates_2)
-
+print(dates_3)
